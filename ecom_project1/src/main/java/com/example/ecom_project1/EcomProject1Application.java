92eaa9627f5600e1a2860464d4ebf8f82920ad92
mayank chauhan <117719876+imayank2@users.noreply.github.com>
2025-01-26 09:54:08 +0530

    Add files via upload

---
+++ ecom_project1/src/main/java/com/example/ecom_project1/EcomProject1Application.java
@@ -0,0 +1,14 @@
+package com.example.ecom_project1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class EcomProject1Application {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext run = SpringApplication.run(EcomProject1Application.class, args);
+	}
+
+}
