ee6e37b4fbd3a7ada4c4b3f4487b71ae6603a554
imayank2 <mayankchauhan8515@gmail.com>
2025-03-05 07:52:03 +0530

    project upload

---
+++ ecom_project1/src/main/java/com/example/ecom_project1/Service/ProductService.java
@@ -0,0 +1,45 @@
+package com.example.ecom_project1.Service;
+
+
+import com.example.ecom_project1.model.Product;
+import com.example.ecom_project1.repo.ProjectRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+
+    @Autowired
+    private ProjectRepo repo;
+    public List<Product> getAllProducts(){
+        return repo.findAll();
+    }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    public Product addProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageDate(imageFile.getBytes());
+        return repo.save(product);
+    }
+
+    public Product updateProduct(int id, Product product, MultipartFile imageFile) throws IOException {
+        product.setImageDate(imageFile.getBytes());
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        return repo.save(product);
+    }
+
+
+    public void deleteProduct(int id) {
+        repo.deleteById(id);
+    }
+}
